d0cc09217e68d2ed77354ffc96ef9de0b1498553
firoliyos <firatesfaye44@gmail.com>
2024-11-08 06:27:44 +0300

    fetch-from-Api

--- food-recipe-app/src/context/index.jsx
+++ food-recipe-app/src/context/index.jsx
@@ -5,8 +5,19 @@ export const GlobalContext = createContext(null)
 export default function GlobalState({ children }) {
 
     const [searchParam, setSearchParam] = useState(null)
+    
+    async function handleSubmit(event) {
+        event.preventDefault()
+         try{
+            const response = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}`)
 
+            const data = await response.json()
+            console.log(data)
+         }catch(error) {
+            console.log(error)
+         }
+    }
     return (
-        <GlobalContext.Provider value={{searchParam, setSearchParam}}>{children}</GlobalContext.Provider>
+        <GlobalContext.Provider value={{searchParam, setSearchParam, handleSubmit}}>{children}</GlobalContext.Provider>
     )
 }
