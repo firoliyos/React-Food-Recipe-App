bb041d0efd26ffc9900e9acc71dee7d95e45b21f
firoliyos <firatesfaye44@gmail.com>
2024-11-08 07:19:21 +0300

    recipes-displayed

--- food-recipe-app/src/components/recipe-item/index.jsx
+++ food-recipe-app/src/components/recipe-item/index.jsx
@@ -1,7 +1,18 @@
 
 
-export default function RecipeItem(item) {
+export default function RecipeItem({item}) {
     return (
-        <div>RecipeItem</div>
+        <div className="flex flex-col w-80 overflow-hidden p-5 bg-white/75 shadow-xl gap-5 border-2 rounded-2xl border-white">
+           <div className="h-40 flex justify-center overflow-hidden items-center rounded-xl">
+            <img 
+             src={item?.image_url}
+             alt="recipe item" 
+             className="block w-full"
+            />
+           </div>
+           <div>
+             <span className="text-sm text-cyan-700 font-medium">{item?.publisher}</span>
+           </div>
+        </div>
     )
 }
