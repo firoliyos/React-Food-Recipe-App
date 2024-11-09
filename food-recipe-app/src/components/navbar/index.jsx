b3f547ef24c543fd4e10dba51b920ada341e2880
firoliyos <firatesfaye44@gmail.com>
2024-11-09 10:50:26 +0300

    Navbar-done

--- food-recipe-app/src/components/navbar/index.jsx
+++ food-recipe-app/src/components/navbar/index.jsx
@@ -1,8 +1,13 @@
-
+import {NavLink} from 'react-router-dom'
 export default function Navbar(){
    return(
     <nav className="flex justify-between items-center py-8 container mx-auto flex-col lg:flex-row gap-5 lg:gap-0 ">
-      <h2 className="text-2xl font-semibold">FOOD RECIPE</h2>
+      <h2 className="text-2xl font-semibold">
+      <NavLink to={'/'} >
+            Food Recipe
+      </NavLink>
+      </h2>
+      
       <form>
        <input 
        type="text" 
@@ -11,6 +16,18 @@ export default function Navbar(){
        className="bg-white/75 p-3 px-8 rounded-full outline-none lg:w-96 shadow-lg shadow-red-100 focus:shadow-red-200"
        />
       </form>
+      <ul className="flex gap-5">
+        <li>
+         <NavLink to={'/'} className="text-black hover:text-gray-700 duration-300">
+            Home
+         </NavLink>
+        </li>
+        <li>
+         <NavLink to={'/favorites'} className="text-black hover:text-gray-700 duration-300">
+            Favorites
+         </NavLink>
+        </li>        
+      </ul>
     </nav>
    )
 }
