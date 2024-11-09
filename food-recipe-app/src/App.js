5e3137fffff0e5ded0a32a7769d1c1b962bf2f59
firoliyos <firatesfaye44@gmail.com>
2024-11-09 09:47:46 +0300

    routing-structure-done-App.js

--- food-recipe-app/src/App.js
+++ food-recipe-app/src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import {Routes,Route} from 'react-router-dom'
  import Navbar from './components/navbar'
+import Home from './pages/home';
+import Favorites from './pages/favorites';
+import Details from './pages/details';
 // import Home from './pages/home'
 // import Favorites from './pages/favorites'
 // import Details from './pages/details'
@@ -10,6 +13,26 @@ function App() {
       <div>
         <div className='min-h-screen p-6 bg-white text-gray-600 text-lg'>
           <Navbar />
+          <Routes>
+            <Route
+            path='/'
+            element={
+              <Home />
+            } 
+            />
+            <Route
+            path='/favorites'
+            element={
+              <Favorites />
+            }
+            />
+            <Route
+            path='/recipe-item/:id'
+            element={
+              <Details />
+            }
+            />
+          </Routes>
         </div>
       </div>
     </div>
