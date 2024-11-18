9979246cff2be2b208470cd38c6fe46f1802d72d
firoliyos <firatesfaye44@gmail.com>
2024-11-18 20:57:58 +0300

    not-much

--- food-recipe-app/src/components/navbar/index.jsx
+++ food-recipe-app/src/components/navbar/index.jsx
@@ -3,11 +3,10 @@ export default function Navbar(){
    return(
     <nav className="flex justify-between items-center py-8 container mx-auto flex-col lg:flex-row gap-5 lg:gap-0 ">
       <h2 className="text-2xl font-semibold">
-      <NavLink to={'/'} >
+       <NavLink to={'/'} >
             Food Recipe
-      </NavLink>
+       </NavLink>
       </h2>
-      
       <form>
        <input 
        type="text" 
